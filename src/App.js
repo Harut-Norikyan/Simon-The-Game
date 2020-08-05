eafde92f1eb08357e3fa60877ff0f38c867eb192
Harut <harutnoro@mail.ru>
2020-08-05 19:53:15 +0400

    end

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ class App extends Component {
       {id: 2, color: 'blue', active: "", number: 2},
       {id: 3, color: 'green', active: "", number: 3},
     ],
-    quantityFunc: 1,
     arr: [],
     myArr: [],
     round: 1,
     level:1,
     firstSecond:900,
     lastSecond:1000,
+    finish:'',
+    active:false
   }
-
 
   mouseDown(m) {
     let {data} = this.state
@@ -37,132 +37,153 @@ class App extends Component {
   }
 
   onClick = (m) => {
-    let {myArr} = this.state
-    myArr.push(m.id)
-    this.setState({
-      myArr
-    })
+    let {myArr,arr,active} = this.state
+    if (active && myArr.length < arr.length){
+      myArr.push(m.id)
+      this.setState({
+        myArr
+      })
+    }
   }
 
-  next = () => {
+  next = async () => {
     let {arr, myArr, round,level} = this.state
     this.setState({
-      myArr: []
+      active:false
     })
-    if (JSON.stringify(arr) === JSON.stringify(myArr)) {
+    if (JSON.stringify(arr) === JSON.stringify(myArr) && arr.length !==0) {
       this.setState({
         round: round + 1,
       })
       this.started()
     } else {
-      console.log("error")
+      this.draw()
+      this.setState({
+        myArr: [],
+        active:true
+      })
     }
-   if (round === 5){
+   if (round >= 10){
      this.setState({
        level:level +1,
-       round: 0
+       round: 1,
      })
    }
-
+   if (round >= 10 && level>=4){
+     this.setState({
+       arr:[],
+       round: 1,
+       level:1,
+       finish:'finish'
+     })
+   }
   }
 
-
-  started = () => {
-    let {arr} = this.state
-    let num = Math.round(Math.random() * 3)
-    arr.push(num)
+  started = async () => {
+    let {arr,level} = this.state
     this.setState({
-      ...arr
+      active:false
     })
-    this.draw()
+    if (arr.length === 10){
+     await this.setState({
+        arr:[],
+      })
+     this.started()
+    }else {
+      if (level === 4){
+        let num = Math.round(Math.random() * 3)
+        arr.unshift(num)
+        this.setState({
+          ...arr
+        })
+        this.draw()
+      }else {
+        let num = Math.round(Math.random() * 3)
+        arr.push(num)
+        this.setState({
+          ...arr
+        })
+        this.draw()
+      }
+    }
+
   }
 
-
-  draw = () => {
-    let {arr, data,level,levelOne,firstSecond,lastSecond} = this.state
+  draw = async () => {
+    let {arr,data,level,firstSecond,lastSecond} = this.state
     if (level === 2){
       this.setState({
         firstSecond: 600,
         lastSecond:800,
       })
     }
-    else if (level === 3){
+    else if (level === 3 || level === 4){
       this.setState({
         firstSecond: 400,
         lastSecond:600,
       })
     }
+    this.setState({
+      myArr: []
+    })
     arr.forEach((el, i) => {
       setTimeout(() => {
         data.map(m => {
           if (m.id === el) {
             m.active = "active"
             this.setState({
-              ...data
+              ...data,
             })
           }
+
         })
         setTimeout(() => {
           data.map(m => {
             if (m.id === el) {
               m.active = ""
               this.setState({
-                ...data
+                ...data,
               })
             }
           })
         }, firstSecond)
-      }, (i + 1) * 1000)
+      }, (i + 1) * lastSecond)
+
+      setTimeout(()=>{
+        this.setState({
+          active:true
+        })
+      },(arr.length + 1)*1000)
+
+    });
+  }
+  
+  handleCheck=async (value)=>{
+   await this.setState({
+     level:+value,
+     round: 1,
+     arr:[],
     })
-    console.log(this.state)
+  }
+  again=()=>{
+    this.setState({
+      finish:""
+    })
   }
 
-
-// started=(quantity)=> {
-  //   let {data, quantityFunc, arr} = this.state
-  //   if (quantity > quantityFunc) {
-  //     this.setState({
-  //       quantityFunc: quantityFunc + 1
-  //     })
-  //     return
-  //   } else {
-  //     setTimeout(() => {
-  //       quantity++
-  //       this.started(quantity);
-  //     }, 1500);
-  //     let num = Math.round(Math.random() * 3)
-  //     console.log(num,"num")
-  //     arr.push(num)
-  //     this.setState({
-  //       ...arr
-  //     })
-  //     data.map(m => {
-  //       if (m.id === num) {
-  //         m.active = "active"
-  //         this.setState({
-  //           ...data
-  //         })
-  //       }
-  //     })
-  //     setTimeout(() => {
-  //       data.map(m => {
-  //         if (m.id === num) {
-  //           m.active = ""
-  //           this.setState({
-  //             ...data
-  //           })
-  //         }
-  //       })
-  //     }, 1000)
-  //   }
-  // }
   render() {
-
-    let {data, round, arr,level} = this.state
-
+    let {data, round, arr,level,myArr,finish} = this.state
     return (
       <>
         <div className="container">
+          <div className={`${finish} end__block`}>
+            <h1
+              className="end__title"
+              onClick={this.again}
+            >
+              CONGRATULATION !!!
+            </h1>
+          </div>
           <div className="title__block">
             <div>
               <h1 className="title">
@@ -184,19 +205,19 @@ class App extends Component {
                 onClick={() => this.onClick(m)}
               >
                 <div className="opacityStyle">
-                  <h1 style={{color: "#fff"}}>{m.number}</h1>
+                  {level !== 4 ?
+                    <h1 style={{color: "#fff"}}>{m.number}</h1>
+                    :null}
                 </div>
               </div>
             )}
           </div>
-
           <div className="accessories">
             <h1 className="level">
-              Level&nbsp;{level}/4
+              Level&nbsp;{level} of 4
             </h1>
-
             <p>
-              Round &nbsp;{round}/5
+              Round &nbsp;{round} of 10
             </p>
             <div className="buttons">
               {arr.length < 1
@@ -208,6 +229,34 @@ class App extends Component {
                 </button>
               }
               <button className="button" onClick={this.next}>Next</button>
+            </div>
+            <div className="myBlock">
+              {level !== 4 && level !== 3?
+               myArr.map(m=><h3 key={m + Math.random()} className="myNumbers">{m}</h3>)
+                :null}
+            </div>
+          </div>
+
+          <div className="inputs">
+            <div className="form-check form-check-inline inputs__block">
+              <input className="form-check-input radio" type="radio" name="inlineRadioOptions" id="gridRadios1"
+                     value={1} onChange={(e)=>this.handleCheck(e.target.value,e.target)} defaultChecked/>
+                <label className="form-check-label label" htmlFor="inlineRadio1">Easy</label>
+            </div>
+            <div className="form-check form-check-inline inputs__block">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="gridRadios2"
+                     value={2} onChange={(e)=>this.handleCheck(e.target.value,e.target)}/>
+                <label className="form-check-label label" htmlFor="inlineRadio2">Normal</label>
+            </div>
+            <div className="form-check form-check-inline inputs__block">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="gridRadios3"
+                     value={3} onChange={(e)=>this.handleCheck(e.target.value,e.target)}/>
+                <label className="form-check-label label" htmlFor="inlineRadio3">Hard</label>
+            </div>
+            <div className="form-check form-check-inline inputs__block">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="gridRadios4"
+                     value={4} onChange={(e)=>this.handleCheck(e.target.value,e.target)}/>
+              <label className="form-check-label label" htmlFor="inlineRadio4">Impossible</label>
             </div>
           </div>
         </div>
